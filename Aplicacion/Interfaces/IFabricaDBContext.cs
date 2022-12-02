c345b7c5cb20ebf8de51b6ad74a3aab6508c2654
Ramon <rsilgal@gmail.com>
2022-12-02 21:08:09 +0100

    Agregadas las interfaces de la aplicacion. IFabrica, ILineaProduccion, IMaquina e ITipoMaquina

---
+++ Aplicacion/Interfaces/IFabricaDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Dominio.Modelos.Confiiguracion;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Interfaces
+{
+    public interface IFabricaDBContext
+    {
+        DbSet<Fabrica> Fabricas { get; set; }
+        Task<List<Fabrica>> GetFabricaAsync();
+        Task<Fabrica> GetFabricaByIdAsync(int id);
+        Task<Fabrica> AddFabricaAsync(Fabrica fabrica);
+        Task<Fabrica> DeleteFabricaAsync(Fabrica fabrica);
+        Task<Fabrica> UpdateFabricaAsync(Fabrica fabrica);
+    }
+}
