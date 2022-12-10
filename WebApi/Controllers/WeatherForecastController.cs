e87ed83e2babb171a7600bc2d146eaf910dcf417
Ramon <rsilgal@gmail.com>
2022-12-10 19:15:26 +0100

    Configuración de la API. - Se configuraa la estructura base de la cadena de conexión con la base de datos. - Agregamos el servicio del DbContext. - Implementamos la injección de dependencias para los módulos de Tickets e Intervenciones.

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -4,13 +4,13 @@ namespace WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,10 @@
+using Aplicacion.Repository;
+using Aplicacion.Services;
+using Aplicacion.Services.Interfaces;
+using Infraestructura;
+using Infraestructura.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<WatchFactoryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration
+    .GetConnectionString("sqlServerDataBase")));
+
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+
+builder.Services.AddScoped<IIntervencionService, IntervencionService>();
+builder.Services.AddScoped<IIntervencionRepository, IntervencionRepository>();
 
 var app = builder.Build();
 
