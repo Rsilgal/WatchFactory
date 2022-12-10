ea519cbda68bf9b4280ad78e318e0a205a1aa8ed
Ramon <rsilgal@gmail.com>
2022-12-10 19:08:20 +0100

    Agregamos el modelo de Intervencion del módulo de Intervenciones.

---
+++ Dominio/Modelos/Intervencion/Intervencion.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Modelos.Configuracion;
+using Dominio.Modelos.Nucleo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Modelos.Intervencion
+{
+    public class Intervencion : EntidadBase<int>
+    {
+        public string Descripcion { get; set; } = string.Empty;
+        public int TicketID { get; set; }
+        public int EstadoIntervencionID { get; set; }
+        public int TipoIntervencionID { get; set; }
+
+        [ForeignKey("Ticket")]
+        public virtual Ticket Ticket { get; set; }
+
+        [ForeignKey("EstadoIntervencionID")]
+        public virtual EstadoIntervencion EstadoIntervencion { get; set; }
+
+        [ForeignKey("TipoIntervencionID")]
+        public virtual TipoIntervencion TipoIntervencion { get; set; }
+    }
+}
