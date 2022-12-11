757a6de3cfeb93de156a5ccfbfdb2933fb0b4c46
Ramon <rsilgal@gmail.com>
2022-12-11 12:42:13 +0100

    Se agregan los controladores de intervenciones y tickets.

---
+++ WebApi/Controllers/IntervencionController.cs
@@ -0,0 +1,60 @@
+﻿using Aplicacion.Services;
+using Dominio.Modelos.Intervencion;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntervencionController : ControllerBase
+    {
+        private readonly IntervencionService _intervencionService;
+
+        public IntervencionController(IntervencionService intervencionService)
+        {
+            _intervencionService = intervencionService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var intervencionesFromService = _intervencionService.GetIntervenciones();
+            if (intervencionesFromService == null) return NotFound();
+            return Ok(intervencionesFromService);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var intervencionFromService = _intervencionService.GetIntervencion(id);
+            if (intervencionFromService == null) return NotFound();
+            return Ok(intervencionFromService);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Intervencion intervencion)
+        {
+            var newIntervencionFromService = _intervencionService.CreateIntervencion(intervencion);
+            if (newIntervencionFromService == null) return NotFound();
+            return Ok(newIntervencionFromService);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(Intervencion intervencion)
+        {
+            var updatedIntervencionFromService = _intervencionService.UpdateIntervencion(intervencion);
+            if (updatedIntervencionFromService == null) return NotFound();
+            return Ok(updatedIntervencionFromService);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var intervencionFromService = _intervencionService.GetIntervencion(id);
+            var deletedIntervencionFromService = _intervencionService.DeleteIntervencion(intervencionFromService);
+            if (deletedIntervencionFromService == null) return NotFound();
+            return Ok(deletedIntervencionFromService);
+        }
+    }
+}
---
+++ WebApi/Controllers/TicketsController.cs
@@ -0,0 +1,60 @@
+﻿using Aplicacion.Services;
+using Dominio.Modelos.Nucleo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly TicketService _ticketService;
+
+        public TicketsController(TicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var ticketsFromService = _ticketService.GetTickets();
+            if (ticketsFromService == null) return NotFound();
+            return Ok(ticketsFromService);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var ticketFromService = _ticketService.GetTicket(id);
+            if (ticketFromService == null) return NotFound();
+            return Ok(ticketFromService);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Ticket ticket)
+        {
+            var newTicketFromService = _ticketService.CreateTicket(ticket);
+            if (newTicketFromService == null) return NotFound();
+            return Ok(newTicketFromService);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(Ticket ticket) 
+        {
+            var updatedTicketFromService = _ticketService.UpdateTicket(ticket);
+            if (updatedTicketFromService == null) return NotFound();
+            return Ok(updatedTicketFromService);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var ticketFromService = _ticketService.GetTicket(id);
+            var deletedTicketFromService = _ticketService.DeleteTicket(ticketFromService);
+            if (deletedTicketFromService == null) return NotFound();
+            return Ok(deletedTicketFromService);
+        }
+    }
+}
