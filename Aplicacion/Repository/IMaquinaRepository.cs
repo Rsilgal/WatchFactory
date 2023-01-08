243ba68fc147b232c55279c98e2d978f227f41ba
Ramon <rsilgal@gmail.com>
2023-01-08 16:51:03 +0100

    Implementacion del repositorio para las Maquinas. (WAT-52)

--- Aplicacion/Repository/IMaquinaRepository.cs
+++ Aplicacion/Repository/IMaquinaRepository.cs
@@ -9,7 +9,13 @@ namespace Aplicacion.Repository
 {
     public interface IMaquinaRepository
     {
-        List<Maquina> GetAllMaquinas();
-        Maquina CreateMaquina(Maquina maquina);
+        Task<List<Maquina>> GetAllMaquinas();
+        Task<List<Maquina>> CreateMaquina(Maquina maquina);
+        Task<List<Maquina>> UpdateMaquina(int id, Maquina maquina);
+        Task<List<Maquina>> DeleteMaquina(Maquina maquina);
+        Task<Maquina> GetMaquinaById(int id);
+        Task<List<Maquina>> GetMaquinasByLinea(int lineaId);
+        Task<List<Maquina>> GetMaquinasByTipoMaquina(int tipoMaquinaId);
+        Task<List<Maquina>> GetMaquinasByFabrica(int fabricaId);
     }
 }
--- Infraestructura/Repository/MaquinaRepository.cs
+++ Infraestructura/Repository/MaquinaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Aplicacion.Repository;
 using Dominio.Modelos.Configuracion;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +17,74 @@ namespace Infraestructura.Repository
             _watchFactory= watchFactory;
         }
 
-        public Maquina CreateMaquina(Maquina maquina)
+        public async Task<List<Maquina>> CreateMaquina(Maquina maquina)
         {
-            throw new NotImplementedException();
+            await _watchFactory.Maquinas.AddAsync(maquina);
+            await _watchFactory.SaveChangesAsync();
+
+            return await GetAllMaquinas();
         }
 
-        public List<Maquina> GetAllMaquinas()
+        public async Task<List<Maquina>> DeleteMaquina(Maquina maquina)
         {
-            throw new NotImplementedException();
+            _watchFactory.Maquinas.Remove(maquina);
+            await _watchFactory.SaveChangesAsync();
+
+            return await GetAllMaquinas();
+        }
+
+        public async Task<List<Maquina>> GetAllMaquinas()
+        {
+            return await _watchFactory.Maquinas.ToListAsync();
+        }
+
+        public async Task<Maquina> GetMaquinaById(int id)
+        {
+            return await _watchFactory.Maquinas.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<List<Maquina>> GetMaquinasByFabrica(int fabricaId)
+        {
+            var maquinas = await _watchFactory.Maquinas.
+                Include(m => m.LineaProduccion).
+                ThenInclude(l => l.Fabrica).
+                Where(m => m.LineaProduccion.Fabrica.Id == fabricaId)
+                .ToListAsync();
+
+            return maquinas;
+        }
+
+        public async Task<List<Maquina>> GetMaquinasByLinea(int lineaId)
+        {
+            var maquinas = await _watchFactory.Maquinas.
+                Include(m => m.LineaProduccion).
+                Where(m => m.LineaProduccion.Id== lineaId).
+                ToListAsync();
+
+            return maquinas;
+        }
+
+        public async Task<List<Maquina>> GetMaquinasByTipoMaquina(int tipoMaquinaId)
+        {
+            var maquinas = await _watchFactory.Maquinas.
+                Include(m => m.TipoMaquina).
+                Where(m => m.TipoMaquina.Id == tipoMaquinaId).
+                ToListAsync();
+
+            return maquinas;
+        }
+
+        public async Task<List<Maquina>> UpdateMaquina(int id, Maquina maquina)
+        {
+            var dbMaquina = await _watchFactory.Maquinas.FirstOrDefaultAsync(m => m.Id ==id);
+
+            dbMaquina.Descripcion = maquina.Descripcion;
+            dbMaquina.TipoMaquinaID = maquina.TipoMaquinaID;
+            dbMaquina.LineaProduccionID = maquina.LineaProduccionID;
+
+            await _watchFactory.SaveChangesAsync();
+
+            return await GetAllMaquinas();
         }
     }
 }
