e86809ff03e4ded93d38214980d6dca277713c09
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:22:48 +0100

    Implementacion de Dto de Maquina. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Maquina/UpdateMaquinaDto.cs
+++ WatchFactory_Client/Models/Dtos/Maquina/UpdateMaquinaDto.cs
@@ -1,6 +1,14 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Maquina
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Maquina
 {
     public class UpdateMaquinaDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int LineaProduccionID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int TipoMaquinaID { get; set; }
     }
 }
