d085cf493389b7132bb76a94b2f930a6967bacad
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:23:11 +0100

    Implementacion del Dto de Permiso. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Permiso/CreatePermisoDto.cs
+++ WatchFactory_Client/Models/Dtos/Permiso/CreatePermisoDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Permiso
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Permiso
 {
     public class CreatePermisoDto
     {
+        [Required, MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; }
     }
 }
--- WatchFactory_Client/Models/Dtos/Permiso/UpdatePermisoDto.cs
+++ WatchFactory_Client/Models/Dtos/Permiso/UpdatePermisoDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Permiso
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Permiso
 {
     public class UpdatePermisoDto
     {
+        [Required, MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; }
     }
 }
