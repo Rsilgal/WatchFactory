3a63a50eccf01458e8f083373d7f9cc09caee4d5
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:24:30 +0100

    Implementacion del Dto de Tipo de maquina. (WAT-48)

--- WatchFactory_Client/Models/Dtos/TipoMaquina/UpdateTipoMaquinaDto.cs
+++ WatchFactory_Client/Models/Dtos/TipoMaquina/UpdateTipoMaquinaDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.TipoMaquina
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.TipoMaquina
 {
     public class UpdateTipoMaquinaDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; }
     }
 }
