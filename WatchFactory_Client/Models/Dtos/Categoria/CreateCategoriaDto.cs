6a793b398dc65c41417ff7fcb0d4e921af07ba06
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:20:35 +0100

    Implentación del Dto de Categoria. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Categoria/CreateCategoriaDto.cs
+++ WatchFactory_Client/Models/Dtos/Categoria/CreateCategoriaDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Categoria
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Categoria
 {
     public class CreateCategoriaDto
     {
+        [MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; }
     }
 }
--- WatchFactory_Client/Models/Dtos/Categoria/UpdateCategoriaDto.cs
+++ WatchFactory_Client/Models/Dtos/Categoria/UpdateCategoriaDto.cs
@@ -1,6 +1,11 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Categoria
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Categoria
 {
     public class UpdateCategoriaDto
     {
+
+        [MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; }
     }
 }
