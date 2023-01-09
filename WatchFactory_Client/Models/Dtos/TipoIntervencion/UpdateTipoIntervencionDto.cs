a9068a40e226c00e977a3aa91f8cd7791f53fdb7
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:24:09 +0100

    Implementacion del Dto de Tipo de intervencion. (WAT-48)

--- WatchFactory_Client/Models/Dtos/TipoIntervencion/UpdateTipoIntervencionDto.cs
+++ WatchFactory_Client/Models/Dtos/TipoIntervencion/UpdateTipoIntervencionDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.TipoIntervencion
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.TipoIntervencion
 {
     public class UpdateTipoIntervencionDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; }
     }
 }
