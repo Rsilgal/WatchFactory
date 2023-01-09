e990b723f89bdd01bdd50c8aabb351c367089616
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:22:25 +0100

    Implementacion del Dto de Linea produccion. (WAT-48)

--- WatchFactory_Client/Models/Dtos/LineaProduccion/CreateLineaDto.cs
+++ WatchFactory_Client/Models/Dtos/LineaProduccion/CreateLineaDto.cs
@@ -1,6 +1,12 @@
-﻿namespace WatchFactory_Client.Models.Dtos.LineaProduccion
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.LineaProduccion
 {
     public class CreateLineaDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; }
+        [Required, Range(0, int.MaxValue)]
+        public int FabricaID { get; set; }
     }
 }
