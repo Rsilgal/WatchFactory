3d262f5e672660404286a62bb2781a2410ce809a
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:25:27 +0100

    Implementacion del Dto de Urgencia. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Urgencia/UpdateUrgenciaDto.cs
+++ WatchFactory_Client/Models/Dtos/Urgencia/UpdateUrgenciaDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Urgencia
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Urgencia
 {
     public class UpdateUrgenciaDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; }
     }
 }
