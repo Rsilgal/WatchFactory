93a5a774523d1a6bc25a724a032ab6b8ba90e6f6
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:21:29 +0100

    Implementación del Dto de Fabrica. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Fabrica/CreateFabricaDto.cs
+++ WatchFactory_Client/Models/Dtos/Fabrica/CreateFabricaDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Fabrica
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Fabrica
 {
     public class CreateFabricaDto
     {
+        [MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; } = string.Empty;
     }
 }
--- WatchFactory_Client/Models/Dtos/Fabrica/UpdateFabricaDto.cs
+++ WatchFactory_Client/Models/Dtos/Fabrica/UpdateFabricaDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Fabrica
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Fabrica
 {
     public class UpdateFabricaDto
     {
+        [MinLength(1), MaxLength(100)]
+        public string Descripcion { get; set; } = string.Empty;
     }
 }
