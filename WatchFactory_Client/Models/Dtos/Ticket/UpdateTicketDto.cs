3d9439ccb4412e0c86e24ae22c87790687b53ddc
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:23:49 +0100

    Implementacion del Dto de Ticket. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Ticket/UpdateTicketDto.cs
+++ WatchFactory_Client/Models/Dtos/Ticket/UpdateTicketDto.cs
@@ -1,6 +1,22 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Ticket
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Ticket
 {
     public class UpdateTicketDto
     {
+        [Required, MinLength(1)]
+        public string Descripcion { get; set; } = string.Empty;
+        [Required, Range(1, int.MaxValue)]
+        public int MaquinaID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int CategoriaID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int UsuarioID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int UrgenciaID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public int ZonaID { get; set; }
+        [Required, Range(1, int.MaxValue)]
+        public string EstadoID { get; set; }
     }
 }
