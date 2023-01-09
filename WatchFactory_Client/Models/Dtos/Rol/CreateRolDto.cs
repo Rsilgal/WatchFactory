5cfbd56574eac6c213d8ee55cd647fb92ffa7ddb
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-09 20:23:27 +0100

    Implementacion del Dto de Rol. (WAT-48)

--- WatchFactory_Client/Models/Dtos/Rol/CreateRolDto.cs
+++ WatchFactory_Client/Models/Dtos/Rol/CreateRolDto.cs
@@ -1,6 +1,12 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Rol
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Rol
 {
     public class CreateRolDto
     {
+        [Required, MinLength(1)]
+        public string Name { get; set; }
+        [Required, MinLength(1)]
+        public string Description { get; set; }
     }
 }
--- WatchFactory_Client/Models/Dtos/Rol/UpdateRolDto.cs
+++ WatchFactory_Client/Models/Dtos/Rol/UpdateRolDto.cs
@@ -1,6 +1,14 @@
-﻿namespace WatchFactory_Client.Models.Dtos.Rol
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchFactory_Client.Models.Dtos.Rol
 {
     public class UpdateRolDto
     {
+        [Required, MinLength(1)]
+        public string Name { get; set; }
+        [Required, MinLength(1)]
+        public string Description { get; set; }
+        [Required]
+        public bool eliminado { get; set; } = false;
     }
 }
