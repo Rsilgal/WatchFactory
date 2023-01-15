015b368a7172eda1752513acaed49e500dfa5df1
Ramon <rsilgal@gmail.com>
2023-01-15 15:58:42 +0100

    Migración de los modelos a la base de datos.

---
+++ WatchFactory/Data/Migrations/20230115145515_DBContext_v1.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchFactory.Data.Migrations
+{
+    public partial class DBContext_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstadoIntervenciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadoIntervenciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fabricas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabricas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permisos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permisos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    eliminado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoIntervenciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoIntervenciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoMaquinas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoMaquinas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Urgencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urgencias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    passwodrHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    passwordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Eliminado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zonas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zonas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lineas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FabricaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lineas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lineas_Fabricas_FabricaID",
+                        column: x => x.FabricaID,
+                        principalTable: "Fabricas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermisoRol",
+                columns: table => new
+                {
+                    PermisosId = table.Column<int>(type: "int", nullable: false),
+                    RolesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermisoRol", x => new { x.PermisosId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_PermisoRol_Permisos_PermisosId",
+                        column: x => x.PermisosId,
+                        principalTable: "Permisos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PermisoRol_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolUsuario",
+                columns: table => new
+                {
+                    RolesId = table.Column<int>(type: "int", nullable: false),
+                    UsuariosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolUsuario", x => new { x.RolesId, x.UsuariosId });
+                    table.ForeignKey(
+                        name: "FK_RolUsuario_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolUsuario_Usuarios_UsuariosId",
+                        column: x => x.UsuariosId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maquinas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LineaProduccionID = table.Column<int>(type: "int", nullable: false),
+                    TipoMaquinaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maquinas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Maquinas_Lineas_LineaProduccionID",
+                        column: x => x.LineaProduccionID,
+                        principalTable: "Lineas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Maquinas_TipoMaquinas_TipoMaquinaID",
+                        column: x => x.TipoMaquinaID,
+                        principalTable: "TipoMaquinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaCierre = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaquinaID = table.Column<int>(type: "int", nullable: false),
+                    CategoriaID = table.Column<int>(type: "int", nullable: false),
+                    UsuarioID = table.Column<int>(type: "int", nullable: false),
+                    UrgenciaID = table.Column<int>(type: "int", nullable: false),
+                    ZonaID = table.Column<int>(type: "int", nullable: false),
+                    EstadoID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Categorias_CategoriaID",
+                        column: x => x.CategoriaID,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Maquinas_MaquinaID",
+                        column: x => x.MaquinaID,
+                        principalTable: "Maquinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Urgencias_UrgenciaID",
+                        column: x => x.UrgenciaID,
+                        principalTable: "Urgencias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Usuarios_UsuarioID",
+                        column: x => x.UsuarioID,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Zonas_ZonaID",
+                        column: x => x.ZonaID,
+                        principalTable: "Zonas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Intervenciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TicketID = table.Column<int>(type: "int", nullable: false),
+                    EstadoIntervencionID = table.Column<int>(type: "int", nullable: false),
+                    TipoIntervencionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Intervenciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Intervenciones_EstadoIntervenciones_EstadoIntervencionID",
+                        column: x => x.EstadoIntervencionID,
+                        principalTable: "EstadoIntervenciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Intervenciones_Tickets_TicketID",
+                        column: x => x.TicketID,
+                        principalTable: "Tickets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Intervenciones_TipoIntervenciones_TipoIntervencionID",
+                        column: x => x.TipoIntervencionID,
+                        principalTable: "TipoIntervenciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Intervenciones_EstadoIntervencionID",
+                table: "Intervenciones",
+                column: "EstadoIntervencionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Intervenciones_TicketID",
+                table: "Intervenciones",
+                column: "TicketID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Intervenciones_TipoIntervencionID",
+                table: "Intervenciones",
+                column: "TipoIntervencionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lineas_FabricaID",
+                table: "Lineas",
+                column: "FabricaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maquinas_LineaProduccionID",
+                table: "Maquinas",
+                column: "LineaProduccionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maquinas_TipoMaquinaID",
+                table: "Maquinas",
+                column: "TipoMaquinaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermisoRol_RolesId",
+                table: "PermisoRol",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolUsuario_UsuariosId",
+                table: "RolUsuario",
+                column: "UsuariosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_CategoriaID",
+                table: "Tickets",
+                column: "CategoriaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_MaquinaID",
+                table: "Tickets",
+                column: "MaquinaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UrgenciaID",
+                table: "Tickets",
+                column: "UrgenciaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UsuarioID",
+                table: "Tickets",
+                column: "UsuarioID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ZonaID",
+                table: "Tickets",
+                column: "ZonaID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Intervenciones");
+
+            migrationBuilder.DropTable(
+                name: "PermisoRol");
+
+            migrationBuilder.DropTable(
+                name: "RolUsuario");
+
+            migrationBuilder.DropTable(
+                name: "EstadoIntervenciones");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "TipoIntervenciones");
+
+            migrationBuilder.DropTable(
+                name: "Permisos");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Maquinas");
+
+            migrationBuilder.DropTable(
+                name: "Urgencias");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Zonas");
+
+            migrationBuilder.DropTable(
+                name: "Lineas");
+
+            migrationBuilder.DropTable(
+                name: "TipoMaquinas");
+
+            migrationBuilder.DropTable(
+                name: "Fabricas");
+        }
+    }
+}
